929fda09f3b65cffc1600344fdd50b5945991c64
Selim <selimchemutai19@gmail.com>
2021-11-08 10:32:02 +0300

    Update MutableFunctionVariable.kt

--- src/Chapter4/FunctionsAsVariables/MutableFunctionVariable.kt
+++ src/Chapter4/FunctionsAsVariables/MutableFunctionVariable.kt
@@ -1,2 +1,9 @@
 package Chapter4.FunctionsAsVariables
 
+var greetingsProvider: () -> String = { "Herro" }//Var makes the function mutable
+
+fun main(args: Array<String>) {
+    println(greetingsProvider())//To print Herro
+    greetingsProvider = { "*Hello" }//The return value can be changed from Hello To Hey
+    println(greetingsProvider())//To Print Hello
+}
