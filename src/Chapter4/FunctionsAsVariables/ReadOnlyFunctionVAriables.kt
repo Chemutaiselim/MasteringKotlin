3a968035394c6eac9fe1b837c48acf25e76cb21b
Selim <selimchemutai19@gmail.com>
2021-11-08 10:22:52 +0300

    Update ReadOnlyFunctionVAriables.kt

--- src/Chapter4/FunctionsAsVariables/ReadOnlyFunctionVAriables.kt
+++ src/Chapter4/FunctionsAsVariables/ReadOnlyFunctionVAriables.kt
@@ -1,6 +1,17 @@
 package Chapter4.FunctionsAsVariables
+
+/*
+The lambda syntax is used to assign greetingProvider as a function
+That returns the literal string hello
+ */
 val greetingProvider: () -> String = { "Hello" }
+
 fun main(args: Array<String>) {
-    var greeting = greetingProvider() // invoke with parentheses
-    greeting = greetingProvider.invoke() // use the invoke method
+    var greeting = greetingProvider()
+    /*
+    greetingProvider will be run twice,
+    The first invocation is done by adding parentheses.
+     */
+    greeting = greetingProvider.invoke()
+    // use the invoke method
 }
